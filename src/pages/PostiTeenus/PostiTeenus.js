7d925458e52c717832b39d8d40d2fd1e27c3fb99
paraIncog <selgeandreas02@gmail.com>
2023-05-24 18:06:31 +0300

    Make backend message visible

--- src/pages/PostiTeenus/PostiTeenus.js
+++ src/pages/PostiTeenus/PostiTeenus.js
@@ -1,42 +1,50 @@
-import { useEffect } from 'react';
-// import PostiDetailid from './PostiDetailid';
+import { useEffect, useState } from 'react';
 
 const app = document.querySelector('#app');
 
 const PostiTeenus = () => {
+    const [matkad,setMatkad] = useState([]);
+
     useEffect(() => {
         fetch('http://localhost:8000')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                console.log('Õnnestus!\n', data);
-                app.innerHTML = `
-                Eesnimi: ${data.eesnimi}<br>
-                Perenimi: ${data.perenimi}<br>
-                Postituse Pealkiri: ${data.title}
-                ${data.desc}
-                `
-            })
-            .catch(function(err) {
-                // handle the error
-                if (err) {
-                    console.log('Ei õnnestunud. Täitsa pekkis!');
-                }
-            });
-            // eslint-disable-next-line react-hooks/exhaustive-deps
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log('Õnnestus!\n', data);
+
+            setMatkad(data)
+
+            // app.innerHTML = `
+            // Eesnimi: ${data.eesnimi}<br>
+            // Perenimi: ${data.perenimi}<br>
+            // Postituse Pealkiri: ${data.title}
+            // ${data.desc}
+            // `
+        })
+        .catch(function(e) {
+            // handle the error
+            if (e) {
+                console.log(`Ei õnnestunud. Täitsa pekkis!\nSiin on viga: ${e}`);
+            }
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
         <div id="row mx-auto text-center">
             <div className='ContentCard HorizontalMargin'>
+                {matkad.map((matk1) => {
+                    return <div key={matk1.title} className='row'>
+                        {matk1.title}
+                    </div>
+                })}
                 <div className='row'>
+
                     {/* Uus postitus */}
                     {/* Logi sisse */}
                     {/* Otsi postitust */}
                     {/* postitused */}
-
-                    {/* <PostiDetailid /> */}
                 </div>
             </div>
         </div>
